f5e8f3173d2db52f491842e4ffff4d87ef706430
Tolihama <fra070@gmail.com>
2023-01-02 20:42:34 +0100

    Import terminal-kit & prompt

--- zelda.js
+++ zelda.js
@@ -1,5 +1,6 @@
 // MODULES
 const fs = require('fs'); // filesystem module
+const term = require( 'terminal-kit' ).terminal;
 
 // ASSETS
 
@@ -12,5 +13,5 @@ fs.readFile('./assets/Start.txt', 'utf-8', printTxt);
 // FUNCTIONS
 function printTxt(err, data) {
     if(err) throw err;
-    console.log(data);
+    term.red(data);
 }
