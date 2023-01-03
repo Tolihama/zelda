4a51b62c48d5f9d459acebd0d2b26bae806a44f0
Francesco Marino <fra070@gmail.com>
2023-01-03 10:27:39 +0100

    Rooms and Items JSON

--- zelda.js
+++ zelda.js
@@ -1,8 +1,12 @@
-// MODULES
+// NODE MODULES
 const fs = require('fs'); // filesystem module
+
+// DEPENDENCIES
 const term = require( 'terminal-kit' ).terminal;
 
 // ASSETS
+const rooms = JSON.parse(fs.readFileSync('./assets/Rooms.txt').toString().trim());
+const items = JSON.parse(fs.readFileSync('./assets/Items.txt').toString().trim());
 
 // GLOBAL VARS
 let currentRoom = 1;
@@ -10,8 +14,14 @@ let currentRoom = 1;
 // STARTS GAME
 fs.readFile('./assets/Start.txt', 'utf-8', printTxt);
 
+
 // FUNCTIONS
 function printTxt(err, data) {
     if(err) throw err;
     term.red(data);
+}
+
+function extractJSONfromTxt(err, data) {
+    if(err) throw err;
+    return JSON.parse(data);
 }
